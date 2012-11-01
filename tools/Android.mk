668f311aaedfa842fa2c24408208a6cf2445a5db
Stephen Smalley <sds@tycho.nsa.gov>
2012-11-01 15:21:36 -0400

    Revert "Moved Android policy tools to tools directory"
    This reverts commit e8ac72be35384f5930af1851e33b4ee9d2114a70.

--- tools/Android.mk
+++ tools/Android.mk
@@ -1,26 +1,3 @@
 LOCAL_PATH:= $(call my-dir)
 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := checkseapp
-LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../libsepol/include/
-LOCAL_CFLAGS := -DLINK_SEPOL_STATIC
-LOCAL_SRC_FILES := check_seapp.c
-LOCAL_STATIC_LIBRARIES := libsepol
-
-include $(BUILD_HOST_EXECUTABLE)
-
-###################################
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := checkfc
-LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../libsepol/include \
-                    $(LOCAL_PATH)/../../libselinux/include
-LOCAL_SRC_FILES := checkfc.c
-LOCAL_STATIC_LIBRARIES := libsepol libselinux
-
-include $(BUILD_HOST_EXECUTABLE)
-
 include $(LOCAL_PATH)/setool/Android.mk
